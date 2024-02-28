94f4d4028f6686e1d84cacf52bd1512666402010
Ercan SUALP <ercansualp@outlook.com.tr>
2024-02-28 22:33:21 +0300

    Added new skills to the 'Skills' section.

--- src/pages/home/components/skills/index.tsx
+++ src/pages/home/components/skills/index.tsx
@@ -46,7 +46,35 @@ export default function Skills() {
                         <div className="flex gap-x-4">
                             <svg stroke="currentColor" fill="#4db5ff" strokeWidth="0" viewBox="0 0 16 16" className="mt-1.5" height="1em" width="1em"><path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" /></svg>
                             <div className="flex flex-col">
+                                <h4 className="font-normal leading-7 text-white">Firebase</h4>
+                                <small className="text-[#ffffff99]">Experienced</small>
+                            </div>
+                        </div>
+                        <div className="flex gap-x-4">
+                            <svg stroke="currentColor" fill="#4db5ff" strokeWidth="0" viewBox="0 0 16 16" className="mt-1.5" height="1em" width="1em"><path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" /></svg>
+                            <div className="flex flex-col">
+                                <h4 className="font-normal leading-7 text-white">Context API</h4>
+                                <small className="text-[#ffffff99]">Experienced</small>
+                            </div>
+                        </div>
+                        <div className="flex gap-x-4">
+                            <svg stroke="currentColor" fill="#4db5ff" strokeWidth="0" viewBox="0 0 16 16" className="mt-1.5" height="1em" width="1em"><path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" /></svg>
+                            <div className="flex flex-col">
+                                <h4 className="font-normal leading-7 text-white">Typescript</h4>
+                                <small className="text-[#ffffff99]">Experienced</small>
+                            </div>
+                        </div>
+                        <div className="flex gap-x-4">
+                            <svg stroke="currentColor" fill="#4db5ff" strokeWidth="0" viewBox="0 0 16 16" className="mt-1.5" height="1em" width="1em"><path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" /></svg>
+                            <div className="flex flex-col">
                                 <h4 className="font-normal leading-7 text-white">Redux</h4>
+                                <small className="text-[#ffffff99]">Experienced</small>
+                            </div>
+                        </div>
+                        <div className="flex gap-x-4">
+                            <svg stroke="currentColor" fill="#4db5ff" strokeWidth="0" viewBox="0 0 16 16" className="mt-1.5" height="1em" width="1em"><path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" /></svg>
+                            <div className="flex flex-col">
+                                <h4 className="font-normal leading-7 text-white">Jquery</h4>
                                 <small className="text-[#ffffff99]">Experienced</small>
                             </div>
                         </div>
